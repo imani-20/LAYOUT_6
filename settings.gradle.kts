b3b11696ac4b47649ecb81ff1ac86b0f2b123581
imani-20 <144381435+imani-20@users.noreply.github.com>
2023-11-07 21:36:56 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LAYOUT_6"
+include(":app")
+ 
